21fc2e27c53f28951c2e8153ac840be084e2fecc
Priyanka Vemulakonda <vemulakonda.priyanka@gmail.com>
2023-10-29 16:10:10 +0530

    Add files via upload

---
+++ background removal/bgremove.py
@@ -0,0 +1,10 @@
+from rembg import remove
+import easygui
+from PIL import Image
+
+inputpath=easygui.fileopenbox(title='select image file')
+outputpath=easygui.filesavebox(title='save file to..')
+
+input=Image.open(inputpath)
+output=remove(input)
+output.save(outputpath)
